f989f1a92ed83c8a735f33bed2fa12b31cdfd6f1
yangxiaobin <yangxiaobin@kuaishou.com>
2021-10-13 17:41:21 +0800

    Fix build break.

--- hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
+++ hugo-plugin/src/main/groovy/hugo/weaving/plugin/HugoPlugin.groovy
@@ -26,10 +26,10 @@ class HugoPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.2-SNAPSHOT'
+      debugCompile 'com.jakewharton.hugo:hugo-runtime:1.2.1'
       // TODO this should come transitively
       debugCompile 'org.aspectj:aspectjrt:1.8.6'
-      compile 'com.jakewharton.hugo:hugo-annotations:1.2.2-SNAPSHOT'
+      compile 'com.jakewharton.hugo:hugo-annotations:1.2.1'
     }
 
     project.extensions.create('hugo', HugoExtension)
@@ -37,6 +37,7 @@ class HugoPlugin implements Plugin<Project> {
     variants.all { variant ->
       if (!variant.buildType.isDebuggable()) {
         log.debug("Skipping non-debuggable build type '${variant.buildType.name}'.")
+        printLog("Skipping non-debuggable build type '${variant.buildType.name}'.")
         return;
       } else if (!project.hugo.enabled) {
         log.debug("Hugo is not disabled.")
@@ -44,10 +45,32 @@ class HugoPlugin implements Plugin<Project> {
       }
 
       JavaCompile javaCompile = variant.javaCompile
+
+      //ajc args: [
+      // -showWeaveInfo,
+      // -1.5,
+      // -inpath,
+      //   /Users/yangxiaobin/DevelopSpace/IDEA/hugo/hugo-example/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes,
+
+      // -aspectpath,
+      //    /Users/yangxiaobin/.gradle/caches/transforms-1/files-1.1/hugo-runtime-1.2.1.aar/a5ace53027ccfdad59bde16452f2b386/jars/classes.jar
+      //   :/Users/yangxiaobin/.gradle/caches/modules-2/files-2.1/com.jakewharton.hugo/hugo-annotations/1.2.1/b01150795c5cdca1eb7e501bf00f105ff0e31501/hugo-annotations-1.2.1.jar
+      //   :/Users/yangxiaobin/.gradle/caches/modules-2/files-2.1/org.aspectj/aspectjrt/1.8.6/a7db7ea5f7bb18a1cbd9f24edd0e666504800be/aspectjrt-1.8.6.jar,
+      //
+      // -d,
+      //    /Users/yangxiaobin/DevelopSpace/IDEA/hugo/hugo-example/build/intermediates/javac/debug/compileDebugJavaWithJavac/classes,
+      //
+      // -classpath,
+      //    /Users/yangxiaobin/.gradle/caches/transforms-1/files-1.1/hugo-runtime-1.2.1.aar/a5ace53027ccfdad59bde16452f2b386/jars/classes.jar
+      //    :/Users/yangxiaobin/.gradle/caches/modules-2/files-2.1/com.jakewharton.hugo/hugo-annotations/1.2.1/b01150795c5cdca1eb7e501bf00f105ff0e31501/hugo-annotations-1.2.1.jar
+      //    :/Users/yangxiaobin/.gradle/caches/modules-2/files-2.1/org.aspectj/aspectjrt/1.8.6/a7db7ea5f7bb18a1cbd9f24edd0e666504800be/aspectjrt-1.8.6.jar,
+      //
+      // -bootclasspath, /Users/yangxiaobin/Library/Android/sdk/platforms/android-29/android.jar]
       javaCompile.doLast {
         String[] args = [
             "-showWeaveInfo",
             "-1.5",
+            "-progress",
             "-inpath", javaCompile.destinationDir.toString(),
             "-aspectpath", javaCompile.classpath.asPath,
             "-d", javaCompile.destinationDir.toString(),
@@ -55,6 +78,7 @@ class HugoPlugin implements Plugin<Project> {
             "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)
         ]
         log.debug "ajc args: " + Arrays.toString(args)
+        printLog("ajc args: " + Arrays.toString(args))
 
         MessageHandler handler = new MessageHandler(true);
         new Main().run(args, handler);
@@ -64,19 +88,28 @@ class HugoPlugin implements Plugin<Project> {
             case IMessage.ERROR:
             case IMessage.FAIL:
               log.error message.message, message.thrown
+              printLog(message.message)
               break;
             case IMessage.WARNING:
               log.warn message.message, message.thrown
+              printLog(message.message)
               break;
             case IMessage.INFO:
               log.info message.message, message.thrown
+              printLog(message.message)
               break;
             case IMessage.DEBUG:
               log.debug message.message, message.thrown
+              printLog(message.message)
               break;
           }
         }
       }
     }
+  }
+
+
+  public void printLog(String msg){
+    println "===> $msg"
   }
 }
